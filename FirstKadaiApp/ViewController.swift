9b48bc352f4e0d12c85349d9ee32e8cb45a619fe
yxyz0926 <yxyz0926@gmail.commkdir ~/.sshtouch ~/.ssh/configchmod 700 ~/.sshchmod 600 ~/.ssh/*xattr -cr ~/.ssh>
2020-12-10 15:28:38 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Yu iwawaki on 2020/12/10.
+//  Copyright © 2020 Yu iwawaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
